d64827bcf558bb7e6693c9b3189b2924a27bfc51
shreya.gr <01fe24ica064@kletech.ac.in>
2025-11-27 16:58:58 +0530

    my first commit

---
+++ array1.py
@@ -0,0 +1,21 @@
+import sys
+
+# Check if user gave arguments
+if len(sys.argv) > 1:
+    scores = sys.argv[1:]
+    print("User-provided scores:")
+else:
+    print("No input given – using default scores")
+    scores = ["85", "90", "78"]
+
+# Convert all scores to integers
+scores = [eval(x) for x in scores]
+
+# Calculate total
+total = sum(scores)
+average = total / len(scores)
+
+# Print results
+print("Scores:", scores)
+print("Total =", total)
+print("Average =", average)
